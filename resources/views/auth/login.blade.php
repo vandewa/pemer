70a24280d0302b0399d44feaafe941a1679973a0
triemaryanto <triemaryanto93@gmail.com>
2023-07-12 13:11:41 +0700

    up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
      <meta name="author" content="Diskominfo Wonosobo">
      <meta name="keyword" content="Sistem Informasi Laporan Kegiatan Harian">
      <link rel="icon" href="{{ asset('snacked/ltr/assets/images/favicon/favicon-32x32.png') }}" type="image/png" />
-     <title>Login LAKON</title>
+     <title>Login Asik Sobo</title>
      <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
      <link href="https://fonts.googleapis.com/css?family=Teko&display=swap" rel="stylesheet">
@@ -24,11 +24,11 @@
                  <div class="mt-4 mb-0 text-center col-md-6">
                      <img src="{{ asset('pemda.png') }}" style="width: 80px;">
                      <h2 class="heading-section">
-                         <span style="margin-left: 10px; font-weight: bold; font-family: 'Teko', sans-serif; color: #ffffff; font-size: 50pt">P
-                            <i class="fa fa-etsy" aria-hidden="true"></i></i> M E R</span>
+                         <span style="margin-left: 10px; font-weight: bold; font-family: 'Teko', sans-serif; color: #ffffff; font-size: 50pt">A
+                             <i class="fa fa-scribd" aria-hidden="true"></i> I K</span>
                      </h2>
                      <span style="margin-left: 10px; font-weight: normal; font-family: 'Teko', sans-serif; color: #ffffff; font-size: 20pt">(
-                         Perjanjian Kerja Online )</span>
+                         Aplikasi Sistem Informasi Kerjasama )</span>
                  </div>
              </div>
              <div class="mt-4 row justify-content-center">
@@ -70,6 +70,17 @@
 								transition: all .15s ease;
 								box-shadow: none;
 								color: #fff;"><b>Register</b></a>
+                             </div>
+                             <div class="col-12 text-center">
+                                 <p class="mb-0">or sign up with:</p>
+                             </div>
+                             <div class="col-12 text-center">
+                                 <div class="error-social">
+                                     <a href="javascript:;" class="bg-facebook"><i class="bx bxl-facebook"></i></a>
+                                     <a href="javascript:;" class="bg-twitter"><i class="bx bxl-twitter"></i></a>
+                                     <a href="javascript:;" class="bg-google"><i class="bx bxl-google"></i></a>
+                                     <a href="javascript:;" class="bg-linkedin"><i class="bx bxl-linkedin"></i></a>
+                                 </div>
                              </div>
                          </form>
                      </div>
