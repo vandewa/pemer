079f15565ec67c3a821dce78dc8173c6d7c013c9
mnurcholis <chollez69@gmail.com>
2024-01-08 09:30:15 +0700

    permission

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,17 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
